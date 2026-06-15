f8abdade37c2d4571f2840391e1aa499c024d61e
tsumiizu <zion.vieira.inf@gmail.com>
2026-06-14 21:39:37 -0300

    debug falso

--- config/settings.py
+++ config/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--)r$23tgx4$m!u%x%gdoum2&wgx@0cv=2+$!ctx0atb!p8l3th'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['andromeda-ogef.onrender.com', '127.0.0.1', 'localhost']
-
+ALLOWED_HOSTS = ['andromeda-jsnt.onrender.com']
+#  ALLOWED_HOSTS = ['127.0.0.1', 'localhost'] endereço local 
 
 # Application definition
 
